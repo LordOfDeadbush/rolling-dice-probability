48690651f4dced5ec206cd6468abb8af4fc6283d
LordOfDeadbush <jcorn2012@gmail.com>
2023-10-23 14:33:23 -0700

    need to fix algo

--- graph.py
+++ graph.py
@@ -1,3 +1,5 @@
+from math import floor
+
 def histo(arr):
     height = max(arr)
 
@@ -11,4 +13,14 @@ def histo(arr):
         print(printstring)
 
 def rolldice(sides, numdice):
-    return 0
+    # min = numdice
+    # max = numdice * sides
+    results = [0 for i in range(sides ** numdice)]
+    for i in range(sides ** numdice):
+        roll_result = 0
+        for j in range(numdice) :
+            roll_result += floor(j/(6 ** j)) % (6 ** (1 - j)) + 1
+        results[roll_result] += 1
+    return results
+
+print(rolldice(6,1))
